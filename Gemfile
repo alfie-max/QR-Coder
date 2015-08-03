c8f0ff65740254ce254064613af47b1c9bbc2455
alfie <alfie.2012@gmail.com>
2015-08-04 00:29:07 +0530

    Setup activeadmin

--- Gemfile
+++ Gemfile
@@ -8,6 +8,8 @@ gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.1.0'
 gem 'therubyracer', platforms: :ruby
+gem 'devise'
+gem 'activeadmin'
 
 gem 'jquery-rails'
 gem 'jbuilder', '~> 2.0'
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root 'application#home'
 
   post  '/qrcode', to: 'application#qrcode', as: 'qrcode'
