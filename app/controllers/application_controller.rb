671809dd8d95fe0d7ab11c6cb10046a3930433e1
alfie <alfie.2012@gmail.com>
2015-08-04 00:27:13 +0530

    Log all ip addresses using service and url's entered

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,8 @@ class ApplicationController < ActionController::Base
 
   def qrcode
     media_link = params[:media_link]
+    QrCode.create(ip_address: request.env.fetch('HTTP_X_FORWARDED_FOR', request.remote_ip), url: media_link)
+
     @qrcode_link = "http://chart.apis.google.com/chart?cht=qr&chs=250x250&choe=UTF-8&chld=H&chl=" + media_link
   end
 end
