bd5c5e6965832b47a979379c981da9868105a463
alfie <alfie.2012@gmail.com>
2015-08-03 22:36:56 +0530

    QR Code Creator Completed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def home
+  end
+
+  def qrcode
+    media_link = params[:media_link]
+    @qrcode_link = "http://chart.apis.google.com/chart?cht=qr&chs=250x250&choe=UTF-8&chld=H&chl=" + media_link
+  end
 end
